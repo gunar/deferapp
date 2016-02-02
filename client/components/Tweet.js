4fbf1c01f65dce9aff26ac4fc811476e503282db
Leonardo Kewitz <leokewitz@gmail.com>
2016-02-02 11:57:45 -0200

    TweetList.

--- client/components/Tweet.js
+++ client/components/Tweet.js
@@ -1,45 +1,32 @@
 import React, { PropTypes } from 'react';
 // import TweetTagList from '../containers/TweetTagList';
 
-// import ListItem from 'material-ui/lib/lists/list-item';
-// import Avatar from 'material-ui/lib/avatar';
-// const Tweet = (t) => (
-//   <ListItem className="tweet"
-//     leftAvatar={<Avatar src={t.user.profile_image_url_https} />}
-//     primaryText={<div>{t.user.name} <span className="screen_name">@{t.user.screen_name}</span></div>}
-//     secondaryText={<p>{t.tweet.text}</p>}
-//     secondaryTextLines={1}
-//   />
-// );
-
-
-import Card from 'material-ui/lib/card/card';
-import CardActions from 'material-ui/lib/card/card-actions';
-import CardHeader from 'material-ui/lib/card/card-header';
-import CardMedia from 'material-ui/lib/card/card-media';
-import CardTitle from 'material-ui/lib/card/card-title';
-import FlatButton from 'material-ui/lib/flat-button';
-import CardText from 'material-ui/lib/card/card-text';
+import ListItem from 'material-ui/lib/lists/list-item';
+import Avatar from 'material-ui/lib/avatar';
 import IconButton from 'material-ui/lib/icon-button';
-import RaisedButton from 'material-ui/lib/raised-button';
-import FontIcon from 'material-ui/lib/font-icon';
+import IconMenu from 'material-ui/lib/menus/icon-menu';
+import MenuItem from 'material-ui/lib/menus/menu-item';
+import Colors from 'material-ui/lib/styles/colors';
+
+const iconButton = (
+  <IconButton
+    touch={true}
+    iconClassName="material-icons"
+    tooltip="archive"
+    iconStyle={{color: Colors.lightGreen600}}
+    tooltipPosition="bottom-left">
+    check
+  </IconButton>
+)
 
 const Tweet = (t) => (
-  <Card className="tweet">
-    <CardHeader
-      title={t.user.name}
-      subtitle={"@"+t.user.screen_name}
-      avatar={t.user.profile_image_url_https}
-      actAsExpander={true}
-      showExpandableButton={true}
-    />
-    {t.media.length > 0 ?
-      <CardMedia expandable={true} overlay={<CardTitle subtitle={t.tweet.text}/>}>
-        <img src={t.media[0]} />
-      </CardMedia>
-      : <CardText className="body" expandable={true}>{t.tweet.text}</CardText>
-    }
-  </Card>
+  <ListItem className="tweet"
+    rightIconButton={iconButton}
+    leftAvatar={<Avatar src={t.user.profile_image_url_https} />}
+    primaryText={<div>{t.user.name} <span className="screen_name">@{t.user.screen_name}</span></div>}
+    secondaryText={<p>{t.tweet.text}</p>}
+    secondaryTextLines={2}
+  />
 );
 
 Tweet.propTypes = {
