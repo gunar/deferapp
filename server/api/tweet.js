63f90229254b6c9920c97c3309f6f411109ab4fb
Gunar C. Gessner <gunar@gunargessner.com>
2016-02-02 00:41:23 -0200

    Make routes for working with multiple tags

--- server/api/tweet.js
+++ server/api/tweet.js
@@ -6,8 +6,8 @@ const User = mongoose.model('User');
 const TagList = mongoose.model('TagList');
 const api = express.Router();
 
-var ENV = process.env.NODE_ENV || 'development';
-var PAGE_LENGTH = 20
+const ENV = process.env.NODE_ENV || 'development';
+const PAGE_LENGTH = 20;
 
 const filterObject = (obj, keys) =>
   Object.keys(obj)
@@ -35,12 +35,20 @@ const unwindTweet = t => {
   ]);
 
   let url = [];
-  if (fullTweet.entities && fullTweet.entities.urls && fullTweet.entities.urls.length) {
+  if (
+    fullTweet.entities
+    && fullTweet.entities.urls
+    && fullTweet.entities.urls.length
+  ) {
     url = fullTweet.entities.urls.map(u => u.expanded_url);
   }
 
   let media = [];
-  if (fullTweet.entities && fullTweet.entities.media && fullTweet.entities.media.length) {
+  if (
+    fullTweet.entities
+    && fullTweet.entities.media
+    && fullTweet.entities.media.length
+  ) {
     media = fullTweet.entities.media.map(m => m.media_url_https);
   }
 
@@ -54,63 +62,65 @@ const unwindTweet = t => {
   };
 };
 
+const getTweetsByTags = (uid, tags, page) => {
+  const match = { 'uid': uid };
+  if (typeof tags === 'object' && tags.length) {
+    match['tags'] = { $all: tags };
+  } else {
+    match['tags'] = { $nin: ['archived'] };
+  }
+
+  return TagList
+          .aggregate({
+            $lookup: {
+              from: 'tweets',
+              localField: 'tid',
+              foreignField: 'tid',
+              as: 'tweet',
+            }
+          })
+          .unwind('tweet')
+          .match(match)
+          .sort('-tid')
+          .skip(parseInt(page, 10) * PAGE_LENGTH)
+          .limit(PAGE_LENGTH)
+          .exec();
+};
+
 api.get('/tweet/:page', passport.authOnly,
   function (req, res, next) {
-    TagList
-      .aggregate({
-        $lookup: {
-          from: 'tweets',
-          localField: 'tid',
-          foreignField: 'tid',
-          as: 'tweet'
-        }
-      })
-      .unwind('tweet')
-      .match({ 'uid': req.user.uid })
-      .sort('-tid')
-      .skip(parseInt(req.params.page)*PAGE_LENGTH)
-      .limit(PAGE_LENGTH)
-      .exec(function (err, docs){
-        if (err) res.status(500).json({message: err});
-        else {
-          //TODO filter fields before sending to frontend
-          // filter out tweets with no links
-          res.json({data: docs.map(unwindTweet)});
-        }
+    getTweetsByTags(req.user.uid, [], req.params.page)
+      .then(docs => {
+        // TODO: filter out tweets with no links
+        console.log(docs);
+        res.json({data: docs.map(unwindTweet)});
+      }, err => {
+        res.status(500).json({message: err});
       });
 });
 
-api.get('/tweet/archive/:page', passport.authOnly,
+api.get('/tweet/:tags/:page', passport.authOnly,
   function (req, res, next) {
-    TagList
-      .aggregate({
-        $lookup: {
-          from: 'tweets',
-          localField: 'tid',
-          foreignField: 'tid',
-          as: 'tweet'
-        }
-      })
-      .unwind('tweet')
-      .match({ 'uid': req.user.uid, 'tags': { $in: ['archived'] } })
-      .sort('-tid')
-      .skip(parseInt(req.params.page)*PAGE_LENGTH)
-      .limit(PAGE_LENGTH)
-      .exec(function (err, docs){
-        if (err) res.status(500).json({message: err});
-        else res.json({data: docs});
+    const tags = req.params.tags.split(',');
+    getTweetsByTags(req.user.uid, tags, req.params.page)
+      .then(docs => {
+        res.json({data: docs.map(unwindTweet)});
+      }, err => {
+        res.status(500).json({message: err});
       });
-});
+  }
+);
 
-api.post('/tweet/archive/:tid', passport.authOnly,
+api.post('/tweet/:tags/:tid', passport.authOnly,
   function (req, res, next) {
+    const tags = req.params.tags.split(',');
     TagList
-      .findOne({tid: req.params.tid, uid: req.user.uid})
+      .findOne({tid: parseInt(req.params.tid, 10), uid: req.user.uid})
       .exec(function (err, tweet) {
         if (err) res.status(500).json({message: err});
         else if (!tweet) res.json({name: "NotFound", message: "Tweet not found."});
         else {
-          tweet.tags.starred.push('archived');
+          tweet.tags.push(...tags);
           tweet.save(function (err, tweet) {
             if (err) res.status(500).json({message: err});
             res.json({data: tweet});
@@ -119,15 +129,16 @@ api.post('/tweet/archive/:tid', passport.authOnly,
       });
 });
 
-api.post('/tweet/unarchive/:tid', passport.authOnly,
+api.delete('/tweet/:tags/:tid', passport.authOnly,
   function (req, res, next) {
+    const tags = req.params.tags.split(',');
     TagList
       .findOne({tid: req.params.tid, uid: req.user.uid})
       .exec(function (err, tweet) {
         if (err) res.status(500).json({message: err});
         else if (!tweet) res.json({name: "NotFound", message: "Tweet not found."});
         else {
-          tweet.tags = tweet.tags.filter(t => t != 'archived');
+          tweet.tags = tweet.tags.filter(t => tags.indexOf(t) == -1);
           tweet.save(function (err, tweet) {
             if (err) res.status(500).json({message: err});
             res.json({data: tweet});
