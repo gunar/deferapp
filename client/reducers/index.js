f2d27ee2be91332ac527a2e9a14449be4f8400c1
Gunar C. Gessner <gunar@gunargessner.com>
2016-02-02 14:16:08 -0200

    Remove unused reducers

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,16 +1,10 @@
 import { combineReducers } from 'redux';
 import tweets from './tweets';
 import loading from './loading';
-// import balance from './balance';
-// import tags from './tags';
-// import filters from './filters';
 
 const rootReducer = combineReducers({
   tweets,
   loading,
-  // balance,
-  // tags,
-  // filters,
 });
 
 export default rootReducer;
