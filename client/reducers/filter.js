c10cf6197908a11f8e74df5b8edd84025cd72fc8
Gunar C. Gessner <gunar@gunargessner.com>
2016-02-02 23:34:53 -0200

    Add TOGGLE_FILTER

--- client/reducers/filter.js
+++ client/reducers/filter.js
@@ -7,6 +7,11 @@ export default function filter(state = [], action) {
     case 'SET_FILTER':
       if (action.filter === "") return []
       return [action.filter];
+    case 'TOGGLE_FILTER':
+      if (state.indexOf('archived') > -1) {
+        return state.filter(f => f != 'archived');
+      }
+      return [...state, 'archived'];
     case 'RESET_FILTER':
       return [];
     default:
