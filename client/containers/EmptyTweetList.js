dc4a104dc3d509c094d963e49178a4a0dd0dad0a
Gunar C. Gessner <gunar@gunargessner.com>
2016-02-11 16:46:48 -0200

    Avoid flash of empty state

--- client/containers/EmptyTweetList.js
+++ client/containers/EmptyTweetList.js
@@ -3,25 +3,33 @@ import { connect } from 'react-redux';
 
 const EmptyTweetList = ({
   isInbox,
-}) => (
-  <div style={{ textAlign: 'center' }}>
+  isLoading,
+  hasTweets,
+}) => {
+  if (isLoading || hasTweets) {
+    return <div />;
+  }
+
+  return (
+    <div style={{ textAlign: 'center' }}>
     <i className={ isInbox ? 'mdi mdi-inbox' : 'mdi mdi-archive'} style={{
       fontSize: 200,
     }}
     ></i>
     { isInbox ?
       <div>
-        <p>This is your inbox.</p>
-        <p>Here you'll find all Tweets you <i className="mdi mdi-heart" /> on Twitter.</p>
-        <div><a target="_blank" href="https://twitter.com">Try now! <i className="mdi mdi-twitter" /></a></div>
+      <p>This is your inbox.</p>
+      <p>Here you'll find all Tweets you <i className="mdi mdi-heart" /> on Twitter.</p>
+      <div><a target="_blank" href="https://twitter.com">Try now! <i className="mdi mdi-twitter" /></a></div>
       </div>
-    :
-      <div>
-        <p>Your archive is empty</p>
-      </div>
+        :
+          <div>
+          <p>Your archive is empty</p>
+          </div>
     }
-  </div>
-);
+    </div>
+  );
+};
 
 EmptyTweetList.propTypes = {
   isInbox: PropTypes.bool.isRequired,
@@ -29,8 +37,12 @@ EmptyTweetList.propTypes = {
 
 const mapStateToProps = (state) => {
   const isInbox = state.filter.length === 0;
+  const isLoading = state.loading;
+  const hasTweets = state.tweets.length > 0;
   return {
     isInbox,
+    isLoading,
+    hasTweets,
   };
 };
 export default connect(mapStateToProps)(EmptyTweetList);
