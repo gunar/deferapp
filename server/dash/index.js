14f2e8bc66040d4a3d8263d493001cf26627580b
Gunar C. Gessner <gunar@gunargessner.com>
2016-02-12 13:56:42 -0200

    Chart working

--- server/dash/index.js
+++ server/dash/index.js
@@ -14,61 +14,64 @@ const x = () => {
 };
 const y = () => (Math.round(Math.random() * 255));
 api.get('/', user.isAdmin, function (req, res, next) {
-  var data = (() => {
-    'use strict';
-    var o = [];
-    for (let i = 0; i <= 20; i++) {
-      o.push({
-        x: x(),
-        y: y()
-      });
-    }
-    return o;
-  })();
-  var charts = [
-    {
-      type: 'line',
-      data: {
-        datasets: [{
-          label: 'Dataset with date object point data',
-          data: data
-        }]
-      },
-      options: {
-        responsive: true,
-        scales: {
-          xAxes: [{
-            type: "time",
-            display: true,
-            scaleLabel: {
+
+  const sendCharts = (datasets) => {
+    var charts = [
+      {
+        type: 'line',
+        data: { datasets: datasets },
+        options: {
+          responsive: true,
+          scales: {
+            xAxes: [{
+              type: "time",
               display: true,
-              labelString: 'Date'
-            }
-          }, ],
-          yAxes: [{
-            display: true,
-            scaleLabel: {
+              scaleLabel: {
+                display: true,
+                labelString: 'Date'
+              }
+            }, ],
+            yAxes: [{
               display: true,
-              labelString: 'value'
-            }
-          }]
+              scaleLabel: {
+                display: true,
+                labelString: 'value'
+              }
+            }]
+          }
         }
       }
-    }
-  ];
+    ];
 
-  const script = s => '<script src="'+s+'"></script>';
-  var head = '<head>'+script('https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js')+script('https://raw.githubusercontent.com/nnnick/Chart.js/v2.0-dev/Chart.min.js')+'</head>';
-  var body = charts.map((c, i) => ('<canvas id="' + i + '" width="'+(c.w || 400)+'" height="'+(c.h || 400)+'"></canvas>'));
-  var chartScript = (() => {
-    const s = charts.map((config, i) => {
-      const ctx = 'document.getElementById('+i+').getContext("2d")'
-      return 'var chart'+i+' = new Chart('+ctx+', '+JSON.stringify(config)+');';
+    const script = s => '<script src="'+s+'"></script>';
+    var head = '<head>'+script('https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js')+script('https://raw.githubusercontent.com/nnnick/Chart.js/v2.0-dev/Chart.min.js')+'</head>';
+    var body = charts.map((c, i) => ('<div style="width: '+(c.w || '80%')+'; height: '+(c.h || '80%')+'"><canvas id="' + i + '"></canvas></div>'));
+    var chartScript = (() => {
+      const s = charts.map((config, i) => {
+        const ctx = 'document.getElementById('+i+').getContext("2d")'
+        return 'var chart'+i+' = new Chart('+ctx+', '+JSON.stringify(config)+');';
+      });
+      return '<script>' + s + '</script>';
+    })();
+    var html = '<html>' + head + body + chartScript + '</html>';
+    res.send(html);
+  }
+
+  Log.find().then(log => {
+    var data = log.map(l => {
+      return {
+        x: l.date,
+        y: 1,
+      };
     });
-    return '<script>' + s + '</script>';
-  })();
-  var html = '<html>' + head + body + chartScript + '</html>';
-  res.send(html);
+    var dataset = {
+      label: 'Gunar requests',
+      data: data
+    };
+
+    sendCharts([dataset]);
+
+  });
 });
 
 module.exports = api;
